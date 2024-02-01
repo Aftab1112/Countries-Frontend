515153a7977f283514fdc890d98a308fadecfe79
Aftab1112 <you@example.com>
2024-02-01 14:52:57 +0530

    displayed all countries along with correct flag and info

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// fetch("https://restcountries.com/v3.1/all")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     data.forEach((country) => {
-//       console.log(country);
-//     });
-//   });
+const countriesContainer = document.querySelector(".countries-container");
 
-// async function fetchCountries() {
-//   const response = await fetch("https://restcountries.com/v3.1/all");
-//   const data = await response.json();
-//   data.forEach((country) => {
-//     console.log(country);
-//   });
-// }
+async function fetchCountries() {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+  data.forEach((country) => {
+    console.log(country);
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
 
-// fetchCountries();
+    countryCard.innerHTML = `
+    <img src=${country.flags.svg} alt="flag" />
+    <div class="card-text">
+      <h3 class="card-tittle">${country.name.common}</h3>
+      <p><b>Population: </b>${country.population.toLocaleString("en-IN")}</p>
+      <p><b>Region: </b>${country.region}</p>
+      <p><b>Capital: </b>${country.capital}</p>
+    </div>
+`;
 
-const countryCard = document.createElement("a");
-countryCard.classList.add("countryCard");
+    countriesContainer.append(countryCard);
+  });
+}
 
-const cardImg = document.createElement("img");
-cardImg.src = "https://flagcdn.com/is.svg";
+fetchCountries();
