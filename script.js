523e59ae1cd6b3b35d637a55f48280702b7486c9
Aftab1112 <you@example.com>
2024-02-04 08:19:10 +0530

    added search filter along with functionality

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const countriesContainer = document.querySelector(".countries-container");
 const filterByRegion = document.querySelector(".filter-by-region");
+const searchInput = document.querySelector(".search-container input");
+let allCountriesData;
 
 async function fetchCountries() {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
+  countriesContainer.innerHTML = "";
+  renderCountries(data);
+  allCountriesData = data;
+}
+
+fetchCountries();
+
+filterByRegion.addEventListener("change", (e) => {
+  async function fetchCountries() {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/region/${filterByRegion.value}`
+    );
+    const data = await response.json();
+    countriesContainer.innerHTML = "";
+    renderCountries(data);
+  }
+  fetchCountries();
+});
+
+function renderCountries(data) {
   data.forEach((country) => {
     const countryCard = document.createElement("a");
     countryCard.classList.add("country-card");
@@ -23,33 +45,10 @@ async function fetchCountries() {
   });
 }
 
-fetchCountries();
-
-filterByRegion.addEventListener("change", (e) => {
-  console.log(filterByRegion.value);
-  async function fetchCountries() {
-    const response = await fetch(
-      `https://restcountries.com/v3.1/region/${filterByRegion.value}`
-    );
-    const data = await response.json();
-    countriesContainer.innerHTML = "";
-    data.forEach((country) => {
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href = `country.html?name=${country.name.common}`;
-
-      countryCard.innerHTML = `
-      <img src=${country.flags.svg} alt="${country.name.common}flag" />
-      <div class="card-text">
-        <h3 class="card-tittle">${country.name.common}</h3>
-        <p><b>Population: </b>${country.population.toLocaleString("en-IN")}</p>
-        <p><b>Region: </b>${country.region}</p>
-        <p><b>Capital: </b>${country.capital ? country.capital[0] : "None"}</p>
-      </div>
-  `;
-
-      countriesContainer.append(countryCard);
-    });
-  }
-  fetchCountries();
+searchInput.addEventListener("input", (e) => {
+  const filteredCountries = allCountriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  countriesContainer.innerHTML = "";
+  renderCountries(filteredCountries);
 });
